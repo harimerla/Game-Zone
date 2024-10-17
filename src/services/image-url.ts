9ec8480695ebf09173c6288d712aca350255139b
harimerla <harikrishnamerla@gmail.com>
2024-10-17 11:48:51 -0500

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from '../asserts/no-image-placeholder-6f3882e0.webp'
+
 const getCroppedImage = (url:string)=>{
-    if(url==null) return '';
+    if(url==null) return noImage;
     const target = 'media/';
     const index = url.indexOf(target)+'media/'.length;
     return url.slice(0,index)+'crop/600/400/'+url.slice(index);
